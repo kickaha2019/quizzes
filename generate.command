ea9160f3b14b5f7745351f550d66d60e6e5654f1
Peter <peter.rootham@alofmethbin.com>
2021-09-01 13:37:17 +0100

    Enable rebuses for Mystery Manor

--- generate.command
+++ generate.command
@@ -1,4 +1,4 @@
 #!/bin/csh
 cd $0:h
 
-ruby -I ruby ruby/generator.rb Definitions/18.yaml /Users/peter/Sites/quizzes/2021-07-99
+ruby -I ruby ruby/generator.rb Definitions/21.yaml /Users/peter/Sites/quizzes/2021-08-22
--- utilities/mystery_manor_quiz.rb
+++ utilities/mystery_manor_quiz.rb
@@ -18,6 +18,7 @@ class MysteryManorQuiz
       @title = prettify( dir.split('/')[-1])
       defn = YAML.load( IO.read( dir + '/meta.yaml'))
       @title = defn['title'] if defn['title']
+      @dir   = dir
 
       if defn['pictures']
         @type   = :pictures
@@ -34,6 +35,7 @@ class MysteryManorQuiz
       @title = prettify( dir.split(/[\.\/]/)[-1])
       defn = YAML.load( IO.read( dir))
       @title = defn['title'] if defn['title']
+      @dir   = File.dirname(dir)
 
       if defn['pairs']
         @type    = :pairs
@@ -44,6 +46,11 @@ class MysteryManorQuiz
       elsif defn['questions']
         @type    = :questions
         @items   = defn['questions'].shuffle.select {|item| was_used( item, index)}.shuffle
+        @answers = @items.collect {|item| item['answer']}
+        @hints   = @answers.sort
+      elsif defn['rebuses']
+        @type    = :rebuses
+        @items   = defn['rebuses'].shuffle.select {|item| was_used( item, index)}.shuffle
         @answers = @items.collect {|item| item['answer']}
         @hints   = @answers.sort
       else
@@ -95,6 +102,9 @@ PROMPT
         io.puts "[img]https://mysterymanor.net/Kickaha/#{item['scaled']}[/img]"
       elsif @type == :questions
         io.puts "[*]#{item['question']}"
+      elsif @type == :rebuses
+        io.puts "\n[size=200]##{index+1}:[/size]"
+        io.puts "[img]https://mysterymanor.net/Kickaha/#{item['image']}[/img]"
       else
         raise "Don't know how to output question for #{dir}"
       end
@@ -111,43 +121,90 @@ PROMPT
     text.gsub( /[_\-]/, ' ')
   end
 
+  def scale_image( picture, target_width, target_height, output)
+    ext = picture.match( /(\..*)$/)[1]
+
+    if not system( "sips -g pixelHeight -g pixelWidth -g orientation " + picture + " >/tmp/sips.log")
+      raise "Error running sips on: " + item[:image]
+    end
+
+    image_height,image_width = 0,0
+    IO.readlines( "/tmp/sips.log").each do |line|
+      if m = /pixelHeight: (\d*)$/.match( line.chomp)
+        image_height = m[1].to_i
+      end
+      if m = /pixelWidth: (\d*)$/.match( line.chomp)
+        image_width = m[1].to_i
+      end
+    end
+
+    raise "Bad image file: #{picture}" unless image_width * image_height > 0
+
+    if (target_width * 1.0) / image_width < (target_height * 1.0) / image_height
+      w = target_width
+      h = image_height * (target_width * 1.0) / image_width
+    else
+      h = target_height
+      w = image_width * (target_height * 1.0) / image_height
+    end
+
+    if ! system( "/bin/csh scale.csh #{picture} #{output}#{ext} #{w.to_i} #{h.to_i}")
+      raise "Error scaling #{picture}"
+    end
+
+    (output + ext)
+  end
+
   def scale_images( target_width, target_height, mm_index, output)
     @items.each_index do |index|
       item = @items[index]
       if item['picture']
         ext = '.jpg'
         ext = '.jpeg' unless File.exist?( item['picture'] + ext)
-        item['scaled'] = "Quiz#{mm_index}#{"ABCDEFGHIJKL"[index..index]}#{ext}"
+        scaled = output + "/Quiz#{mm_index}#{"ABCDEFGHIJKL"[index..index]}"
+        item['scaled'] = scale_image( item['picture'] + ext, target_width, target_height, scaled)
+      elsif item['rebus']
+        parts = []
 
-        if not system( "sips -g pixelHeight -g pixelWidth -g orientation " + item['picture'] + ext + " >/tmp/sips.log")
-          raise "Error running sips on: " + item[:image]
-        end
-
-        image_height,image_width = 0,0
-        IO.readlines( "/tmp/sips.log").each do |line|
-          if m = /pixelHeight: (\d*)$/.match( line.chomp)
-            image_height = m[1].to_i
+        item['rebus'].split( /\s/).each do |element|
+          if /^[a-z]*$/i =~ element
+            parts << scale_image( @dir + '/images/' + element + '.jpg',
+                                  1000,
+                                  target_height,
+                                  "/tmp/#{parts.size}")
+          elsif m = /^([a-z])->([a-z])$/i.match( element)
+            parts << scale_image( @dir + '/symbols/' + m[1] + '.jpg',
+                                  1000,
+                                  target_height,
+                                  "/tmp/#{parts.size}")
+            parts << scale_image( @dir + '/symbols/replace.jpg',
+                                  1000,
+                                  target_height,
+                                  "/tmp/#{parts.size}")
+            parts << scale_image( @dir + '/symbols/' + m[2] + '.jpg',
+                                  1000,
+                                  target_height,
+                                  "/tmp/#{parts.size}")
+          elsif m = /^+([a-z])$/i.match( element)
+            parts << scale_image( @dir + '/symbols/' + m[1] + '.jpg',
+                                  1000,
+                                  target_height,
+                                  "/tmp/#{parts.size}")
+          elsif m = /^-([a-z])$/i.match( element)
+            parts << scale_image( @dir + '/symbols/' + m[1] + '_.jpg',
+                                  1000,
+                                  target_height,
+                                  "/tmp/#{parts.size}")
+          else
+            raise "Unhandled rebus element: #{element}"
           end
-          if m = /pixelWidth: (\d*)$/.match( line.chomp)
-            image_width = m[1].to_i
-          end
         end
 
-        raise "Bad image file: #{item['picture']}" unless image_width * image_height > 0
-
-        if (target_width * 1.0) / image_width < (target_height * 1.0) / image_height
-          w = target_width
-          h = image_height * (target_width * 1.0) / image_width
-        else
-          h = target_height
-          w = image_width * (target_height * 1.0) / image_height
+        if ! system( '/bin/csh merge.csh ' +
+                     "#{output}/Quiz#{mm_index}#{"ABCDEFGHIJKL"[index..index]}.jpg " +
+                     parts.join( ' '))
+          raise "Error merging to #{picture}"
         end
-
-        if ! system( "/bin/csh scale.csh #{item['picture']}#{ext} #{output}/#{item['scaled']} #{w.to_i} #{h.to_i}")
-          raise "Error scaling #{item['picture']}"
-        end
-
-        item['ext'] = ext
       end
     end
   end
